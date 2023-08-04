c83bc2a07f3bf32227baaf941efe1cf4b8a05cae
ben <ndaloben8@gmail.com>
2023-08-04 18:08:38 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}  
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App Title'),
+        ),
+        body: const Text('Body'),
+
+        bottomNavigationBar:BottomNavigationBar(
+          items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home)
+          ),
+          BottomNavigationBarItem(
+            label: 'Timer',
+            icon: Icon(Icons.av_timer))
+        ]),
+      ),
+         
+    );
+  }
+}

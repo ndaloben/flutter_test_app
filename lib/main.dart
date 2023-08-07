c2299b298837500e40ec08290a3acd4c0c2aa110
ben <ndaloben8@gmail.com>
2023-08-07 09:58:11 +0300

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'dart:math';
+
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 
@@ -6,7 +7,6 @@ void main() {
   runApp(const MyApp());
 }  
 
-// ignore: must_be_immutable
 class MyApp extends StatefulWidget {
    const MyApp({super.key});
 
@@ -15,46 +15,72 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String buttonName = 'click';
-  int currentIndex = 0;
+  
   
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: FirstPage(),
+        
+    );
+  }  
+}
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  String buttonName = 'Next Page';
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: const Text('App Title'),
+          
         ),
-        body: Center(child: SizedBox(
+        body: Center(child: currentIndex == 0 ? Container(
           width: double.infinity,
           height: double.infinity,
+          color: Colors.blue,
+          
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.amber,
+                ),
                 onPressed: () {
-                  setState(() {
-                    buttonName = ('Clicked');
-                  });
+                 Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
+                  return const SecondPage();
+                 },
+                 ),
+                 );
                   
                 },
                 child: Text(buttonName),
               ),
                ElevatedButton(
                 onPressed: () {
-                  setState(() {
-                    buttonName = ('Clicked');
-                  });
+                 
                   
                 },
                 child: Text(buttonName),
               ),
             ],
           ),
-        ),
+        ) 
+        : Image.asset('images/bite.png'),
         ),
 
         bottomNavigationBar:BottomNavigationBar(
@@ -77,8 +103,25 @@ class _MyAppState extends State<MyApp> {
           });
         },
         ),
+      );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
       ),
-        
+      body: const Center(
+        child: Card(
+          color: Colors.green,
+          child: Text('This is a flutter test app'),
+          ),
+      ),
     );
-  }  
+  }
 }
